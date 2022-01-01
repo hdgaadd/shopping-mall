51bd1010dc9080fac2495a8e57d1e736de480cd6
CodeChen <53108370+hdgaadd@users.noreply.github.com>
2022-01-01 23:38:32 +0800

    update

--- admin/src/main/java/com/codeman/controller/pms/ProductCategoryController.java
+++ admin/src/main/java/com/codeman/controller/pms/ProductCategoryController.java
@@ -24,6 +24,12 @@ public class ProductCategoryController {
     @Resource
     private ProductCategoryService productCategoryService;
 
+    // CREATE READ UPDATE DELETE
+
+    // create(ProductCategory productCategory) 创建商品分类，如果传入的商品分类对象没有父类id则设置其为最高级别商品分类，有则level设置为父类的level - 1
+
+    // update(Long id, ProductCategoryParam param) 根据商品分类id，更新商品分类，若该param拥有List<商品属性id>，则删除该商品分类的原有所有属性，把新的属性id插入
+
     @ApiOperation("获取所有的一级分类和二级分类")
     @GetMapping("listWithChildren")
     public CommonResult<List<ProductCategoryWithChildrenItem>> listWithChildren() {
